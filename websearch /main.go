499ba75173e84fbb23cbe6b7a012d562af86c3df
JasonAlbertEinstien <j2004nol@gmail.com>
2025-06-16 23:06:23 +0800

    main flow of websearch

--- websearch /main.go
+++ websearch /main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,13 +11,20 @@ type Message struct {
 	Query string `json:"query"`
 }
 
+func GetSearchResult() {}
+
+// check if the server is healthy
 func Health(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"status": true,
 	})
 }
 
+/*
+The shearch function
+*/
 func SearchContent(c *gin.Context) {
+	/**/
 	var json Message
 
 	if err := c.ShouldBindJSON(&json); err != nil {
@@ -29,6 +37,10 @@ func SearchContent(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": json.Query,
 	})
+	go func() {
+		time.Sleep(5 * time.Second)
+		fmt.Println("Hello world")
+	}()
 
 }
 
