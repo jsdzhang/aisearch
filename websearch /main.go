5cae1b5846305ef15dc2f88eff92816df2df3926
JasonAlbertEinstien <j2004nol@gmail.com>
2025-06-16 23:55:32 +0800

    get search result =

--- websearch /main.go
+++ websearch /main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -15,14 +19,50 @@ type Message struct {
 	Query string `json:"query"`
 }
 
-func GetSearchResult() {}
-
-// check if the server is healthy
-func Health(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"status": true,
-	})
+type Result struct {
+	Url     string `json:"url"`
+	Snippet string `json:"snippet"`
+	Content string `json:"content"`
+	Summary string `json:"summary"`
 }
+
+func GetSearchResult(q string) (string, error) {
+
+	url := "https://google.serper.dev/search"
+	method := "POST"
+	playload := strings.NewReader(`{"q":"` + q + `"}`)
+
+	fmt.Println(playload)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, playload)
+
+	if err != nil {
+		slog.Error(err.Error())
+		return "", err
+	}
+
+	req.Header.Add("X-API-KEY", SERPER_API)
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		slog.Error(err.Error())
+		return "", err
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		slog.Error(err.Error())
+		return "", err
+	}
+
+	return string(body), nil
+}
+
+func AddSearchResult() {}
 
 /*
 The shearch function
@@ -32,13 +72,13 @@ func SearchContent(c *gin.Context) {
 		Handle the search here
 	*/
 	var json Message
+	//search query handler
+	GetSearchResult("apple inc")
 
 	if err := c.ShouldBindJSON(&json); err != nil {
 		c.JSON(400, gin.H{"error": "Invalid JSON"})
 		return
 	}
-
-	fmt.Println(SERPER_API)
 
 	c.JSON(200, gin.H{
 		"message": json.Query,
@@ -46,11 +86,17 @@ func SearchContent(c *gin.Context) {
 	// POST handling
 	go func() {
 		time.Sleep(5 * time.Second)
-		fmt.Println(json.Query)
+		//fmt.Println(json.Query)
 	}()
 }
 
 // initial set up , API & DB
+
+func Health(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"status": true,
+	})
+}
 
 func main() {
 	err := godotenv.Load()
