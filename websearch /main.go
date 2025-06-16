12332e6dd80b1207bffbd054f558a98a6d0013ea
JasonAlbertEinstien <j2004nol@gmail.com>
2025-06-16 22:48:10 +0800

    websearch health

--- websearch /main.go
+++ websearch /main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/gin-gonic/gin"
+)
+
+type Message struct {
+	Query string `json:"query"`
+}
+
+func Health(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"status": true,
+	})
+}
+
+func SearchContent(c *gin.Context) {
+	var json Message
+
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid JSON"})
+		return
+	}
+
+	fmt.Println(json.Query)
+
+	c.JSON(200, gin.H{
+		"message": json.Query,
+	})
+
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/health", Health)
+	router.POST("/ping", SearchContent)
+	router.Run()
 }
